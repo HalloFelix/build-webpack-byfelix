25ff2f8116ce5a8ac2d35b96adf2747fe421a416
FelixChan <741883227@qq.com>
2020-04-27 01:45:38 +0800

    chroe: update travis-ci--6

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,8 +3,6 @@ describe('webpack.base.js test case', () => {
   const baseConfig = require('../../lib/webpack.base')
 
   it('entry', () => {
-    console.log(baseConfig.entry.index)
-    build-webpack-byfelix
     assert.equal(baseConfig.entry.index.indexOf('build-webpack-byfelix/test/smoke/template/src/index/index.js') > -1, true)
     assert.equal(baseConfig.entry.search.indexOf('build-webpack-byfelix/test/smoke/template/src/search/index.js') > -1, true)
   })
